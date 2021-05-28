5d87911558f650e4518e7df40e0b2d8ba1a94712
Guillen, Nicolas <n.guillen.chaves@accenture.com>
2021-05-27 22:44:26 -0600

    🗑 delete postman endpoint with log generation

--- server.js
+++ server.js
@@ -14,24 +14,6 @@ app.get('/api/appointments', async (req, res) => {
   }
 })
 
-app.get('/api/appointments/postman', async (req, res) => {
-  try {
-    var results = await query()
-    const fs = require('fs')
-    const content = JSON.stringify(results, null, 2)
-    const { format } = require('date-fns')
-    const logName = `${ format(Date.now(), 'y-L-d-HH') }.txt`
-    fs.writeFile(`./public/${logName}`, content, err => {
-      if (err) {
-        return res.status(500).json(err)
-      }
-      return res.status(400).json(results)
-    })
-  } catch (err) {
-    return res.status(500).json(err)
-  }
-})
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
